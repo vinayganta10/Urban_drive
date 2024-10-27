73d84d7769c12539c8119a9c4748b55025c6946d
vinayganta <aggvinayganta10@gmail.com>
2024-10-27 09:05:39 +0530

    minor changes

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,11 +3,15 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import HomeComponent from './HomeComponent';
+import Login from './components/auth/login.js';
+import Signup from './components/auth/signup.js';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <BrowserRouter>
         <Routes>
           <Route path="/" exact index element={<HomeComponent/>} />
+          <Route path="/login" element={<Login/>} />
+          <Route path="/signup" element={<Signup/>} />
         </Routes>
     </BrowserRouter>
 );
